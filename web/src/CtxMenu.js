ef3f154078036c5f87ce0008a9dabfd08cfab617
Peter Kagstrom <peter.kagstrom@gmail.com>
2021-03-24 00:42:03 -0400

    Updated icons for abstract, io, config, and edit

--- web/src/CtxMenu.js
+++ web/src/CtxMenu.js
@@ -1,38 +1,40 @@
 import { cy } from './Graph'
 import render from 'mithril-node-render'
 import m from 'mithril'
+import { newNode } from './Moe'
 
 export var menuOptions = {
 					selector: 'node, edge',
 
 					commands: [
 						{
-							content: render.sync(m('.material-icons', {}, 'functions')),
+							content: render.sync(m('.material-icons', {}, 'filter_b_and_w')),
 							select: function(ele){
 								console.log( ele.id() );
 							},
 						},
 
 						{
-							content: render.sync(m('.material-icons', {}, 'save')),
+							content: render.sync(m('.material-icons', {}, 'swap_horiz')),
 							select: function(ele){
 								console.log( ele.data('name') );
 							},
 						},
 
 						{
-							content: render.sync(m('.material-icons', {}, 'swap_horiz')),
+							content: render.sync(m('.material-icons', {}, 'miscellaneous_services')),
 							select: function(ele){
 								console.log( ele.data('io') );
 							},
 						},
 
 						{
-							content: 'Text',
+							content: render.sync(m('.material-icons', {}, 'mode_edit')),
 							select: function(ele){
-								console.log( ele.position() );
+								console.log( 'clicked',  ele.data());
+                cy.add(newNode('null'))
 							},
-							enabled: false
+							enabled: true
 
 						}
 					]
