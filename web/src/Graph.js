2aaa54f03318757799917176d2b91c9e25e4f8f9
Harpo <harpo@harpo.me>
2022-08-21 10:46:46 -0700

    Prettier web files

--- web/src/Graph.js
+++ web/src/Graph.js
@@ -3,31 +3,39 @@ import render from 'mithril-node-render'
 
 import cytoscape from 'cytoscape'
 import edgehandles from 'cytoscape-edgehandles'
-cytoscape.use( edgehandles )
+cytoscape.use(edgehandles)
 import dagre from 'cytoscape-dagre'
-cytoscape.use( dagre )
+cytoscape.use(dagre)
 import klay from 'cytoscape-klay'
-cytoscape.use( klay )
+cytoscape.use(klay)
 import coseBilkent from 'cytoscape-cose-bilkent'
-cytoscape.use( coseBilkent )
+cytoscape.use(coseBilkent)
 import cxtmenu from 'cytoscape-cxtmenu'
-cytoscape.use( cxtmenu )
+cytoscape.use(cxtmenu)
 
 import { style } from './style.js'
 import '../node_modules/material-design-icons-iconfont/dist/material-design-icons.css'
 
 import { menuOptions } from './CtxMenu'
 import { moedata, id, newNode } from './Moe'
-import { testPosition, orderRadially, node, edge, graph} from './Collections'
+import { testPosition, orderRadially, node, edge, graph } from './Collections'
 
 export var cy, eh, elements
 
-let sortAlgos = ['grid', 'circle', 'concentric', 'random', 'dagre', 'klay', 'cose-bilkent' ]
+let sortAlgos = [
+  'grid',
+  'circle',
+  'concentric',
+  'random',
+  'dagre',
+  'klay',
+  'cose-bilkent',
+]
 
 export function Graph() {
   return {
     oncreate: vnode => {
-			console.log('cy init')
+      console.log('cy init')
       cy = cytoscape({
         container: vnode.dom,
         elements: moedata,
@@ -36,35 +44,55 @@ export function Graph() {
         //      },
         style: style,
       })
-			let menu = cy.cxtmenu(menuOptions)
+      let menu = cy.cxtmenu(menuOptions)
 
+      cy.add({
+        data: {
+          id: id(),
+          name: 'moe',
+          x: window.innerWidth / 2,
+          y: window.innerHeight / 2,
+        },
+      }).on('tap', function (e) {
+        orderRadially()
+      })
       cy.add(
-        {data: { id:id(), name:'moe', x:window.innerWidth/2, y:window.innerHeight/2}}
-          ).on('tap', function(e){orderRadially()})
-      cy.add(
-        cy.add({data: { id:id(), name:'node', x:window.innerWidth/2, y:window.innerHeight/2}}
-      ).on('tap', function(e){cy.add(newNode (Date(), e))})
+        cy
+          .add({
+            data: {
+              id: id(),
+              name: 'node',
+              x: window.innerWidth / 2,
+              y: window.innerHeight / 2,
+            },
+          })
+          .on('tap', function (e) {
+            cy.add(newNode(Date(), e))
+          })
       )
       orderRadially()
-      },
+    },
     view: vnode => {
-      return m('.graph', {}, )
-    }
+      return m('.graph', {})
+    },
   }
 }
-
 
 export function Selector() {
   return {
     view: vnode => {
       return [
         m('label.formlabel', { for: vnode.attrs.name }, vnode.attrs.text),
-        m('select', vnode.attrs, vnode.children.map(c => {
-          return m('option', {value: c}, c)
-        })),
+        m(
+          'select',
+          vnode.attrs,
+          vnode.children.map(c => {
+            return m('option', { value: c }, c)
+          })
+        ),
         m('br'),
       ]
-    }
+    },
   }
 }
 
@@ -72,20 +100,24 @@ export function GraphSelector() {
   return {
     view: vnode => {
       return [
-				m(Selector, {
-					oninput: e => {
-						let layout = cy.layout({
-							name: e.target.value,
-							padding: 5,
-							animate: true,
-							nodeDimensionsIncludeLabels: true,
-						})
-						layout.run()
-					}
-				}, sortAlgos),
+        m(
+          Selector,
+          {
+            oninput: e => {
+              let layout = cy.layout({
+                name: e.target.value,
+                padding: 5,
+                animate: true,
+                nodeDimensionsIncludeLabels: true,
+              })
+              layout.run()
+            },
+          },
+          sortAlgos
+        ),
 
-				m(Graph)
-			]
-    }
+        m(Graph),
+      ]
+    },
   }
 }
--- web/src/index.js
+++ web/src/index.js
@@ -3,25 +3,23 @@ import { Layout } from './Menu'
 import { GraphSelector } from './Graph'
 import './style.css'
 
-
 export function Button() {
   return {
     view: vnode => {
       return m('input[type=submit].button', vnode.attrs, vnode.children)
-    }
+    },
   }
 }
 
 export function Home() {
-	return {
-		view: vnode => {
-			return m('moe')
-		}
-	}
+  return {
+    view: vnode => {
+      return m('moe')
+    },
+  }
 }
 
-
-m.route(document.body, "/", {
-  '/': { render: () => m(Layout, m(Home))},
-	'/graph': { render: () => m(Layout, m(GraphSelector))},
+m.route(document.body, '/', {
+  '/': { render: () => m(Layout, m(Home)) },
+  '/graph': { render: () => m(Layout, m(GraphSelector)) },
 })
