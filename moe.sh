2c0ad952e1d938ee572929435a81acfbff2f824e
Harpo <harpo@harpo.me>
2022-08-22 14:57:39 -0700

    Use newer docker compose format

--- moe.sh
+++ moe.sh
@@ -17,17 +17,17 @@ elif [ $1 = "build" ]; then
 elif [[ $1 = "run" || $1 =~ "serve"  || $1 =~ "dev" ]]; then
   shift
   # uvicorn --reload --host 0.0.0.0 --port 5000 main:app
-  docker-compose -f docker-compose.yml -f dev.yml up "$@"
+  docker compose -f docker-compose.yml -f dev.yml up "$@"
 
 # Starts in production.
 elif [ $1 = "prod" -o $1 = "production" -o $1 = "p" ]; then
   shift
   echo "moe running production $1"
-  docker-compose down && \
-  docker-compose -f docker-compose.yml -f production.yml up --build -d $1
+  docker compose down && \
+  docker compose -f docker-compose.yml -f production.yml up --build -d $1
 
 elif [ $1 = "log" -o $1 = "l" ]; then
   shift
-  docker-compose logs "$@"
+  docker compose logs "$@"
 
 fi
